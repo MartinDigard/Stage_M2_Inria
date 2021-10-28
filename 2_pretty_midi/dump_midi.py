9dacceef8274ab55509f1758dd0629f7b99d7aa8
Martin Digard <martindigard@gmail.com>
2021-10-28 20:56:41 +0200

    2_pretty_midi/

--- 2_pretty_midi/dump_midi.py
+++ 2_pretty_midi/dump_midi.py
@@ -2,24 +2,39 @@
 # python 3.8
 
 """
-Dump un fichier MIDI en texte au format :
-point, secondes, ON/OFF, pitch, velocité
+Dumpe un fichier MIDI en texte au format :
+Date, Set(ON/OFF), Pitch, Velocité.
 
-usage: dump_midi.py [-h] midi_input midi_output
+Usage : dump_midi.py [-h] midi_input midi_output
 
 Testé avec la commande :
-python3 dump_midi.py 3bars_fill_groove-016.mid midi_output
+python3 dump_midi.py midi_test.mid output
 """
 
-import os
 import argparse
-import pretty_midi
+import re
+import os
+import pretty_midi as pm
+
+
+def format_event(resolution, date, on_off_set, pitch, velocite):
+    """
+    Entrée : resolution — int, le rapport pour l’équivalence entre le tick et
+             les secondes ;
+             on_off_set — string ;
+             pitch — int ;
+             velocite — int.
+    Retourne la ligne au format « Date, Set(ON/OFF), Pitch, Velocité. »
+    """
+    date = str(round(date / resolution, 6))
+    date = date + ' ' * (8 - len(date))
+    return f"Date : {date}\tSet={on_off_set}\tKey={pitch}\tVel={velocite}"
+
 
 def main():
     """
-    Entrée : Un fichier midi.
-    Sortie : Le fichier midi en texte au format
-             « point[n]: date_en_seconde ON/OFF key=pitch vel=vélocité »
+    Entrée : Un fichier midi ;
+    Sortie : Le fichier midi en texte.
     """
 
     # Argument
@@ -29,25 +44,35 @@ def main():
     args = parser.parse_args()
     midi_input = args.midi_input
     midi_output = args.midi_output
-    midi = pretty_midi.PrettyMIDI(midi_input)
+    midi = pm.PrettyMIDI(midi_input)
+    dates = {}
+    dates_ordonnees = []
 
+    # Traitement des évènements pour le dump
+    for instrument in midi.instruments:
 
-    # Pré-traitement
-    for instruments in midi.instruments:
-        for events in instruments.notes:
-            print(events)
-    #division = int(midi_input.split('\n')[0].split()[3].split('=')[1])
-    #lines = [line for line in midi_input.split('\n')
-    #         if line.startswith("Time")
-    #         and line.split()[1].startswith("Note")]
-    #events = [f"point[{num}]: {line}" for num, line in enumerate(lines)]
+        # Récupérer (date, ON-OFF, pitch, vélocité) et ordonner par date
+        for events in instrument.notes:
+            dates[midi.time_to_tick(events.start)] = ("ON", events.pitch,
+                                                      events.velocity)
+            dates[midi.time_to_tick(events.end)] = ("OFF", events.pitch,
+                                                    0)
+        for k in sorted(dates.keys()):
+            date = ("%s : %s" % (k, dates[k]))
+            date = ' '.join(' '.join(date.split(' : (')).split(', '))
+            date = re.sub(r'\)', '', date)
+            date = date.split()
+            on_off_set = re.sub('\'', '', date[1])
+            date = (int(date[0]), on_off_set, int(date[2]), int(date[3]))
+            dates_ordonnees.append(date)
 
     # Formatage ligne par ligne et écriture dans un fichier de sortie
     if os.path.exists(midi_output):
         os.remove(midi_output)
-    # with open(midi_output, 'a') as output:
-    #     for line in midi:
-    #         output.write(f"{format_event(line, division)}\n")
+    with open(midi_output, 'a') as output:
+        for date in dates_ordonnees:
+            output.write(f"""{format_event(midi.resolution, date[0], date[1],
+                              date[2], date[3])}\n""")
 
 
 if __name__ == "__main__":
