9fcffe8e44594f69390aaef5ed2312bb4e8b112d
Martin Digard <martindigard@gmail.com>
2021-10-29 17:30:56 +0200

    3_music21/

---
+++ 3_music21/a_mei_to_score.py
@@ -0,0 +1,19 @@
+"""
+Convertit un fichier MEI en partition…
+"""
+
+import argparse
+from music21 import mei
+
+# Paramètres
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file')
+args = parser.parse_args()
+
+# Lecture MEI
+with open(args.input_file) as input_f:
+    mei_file = input_f.read()
+    conv = mei.MeiToM21Converter(mei_file)
+    result = conv.run()
+print(result.show('text'))
+print(result.show())
